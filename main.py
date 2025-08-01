232bc403bd93004878791143c825ae2411f8dcd1
vizz1234 <vishdd4@gmail.com>
2025-08-01 21:53:47 +0530

    completed epsilon greedy

--- main.py
+++ main.py
@@ -2,45 +2,41 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 import redis
-import random
+from eGreedy import eGreedy
 
 app = FastAPI()
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 templates = Jinja2Templates(directory="templates")
 
-VARIANTS = ["A", "B"]
+VARIANTS = {
+    "A": 0,
+    "B": 1,
+    "C": 2
+}
 EPSILON = 0.1
-
-def get_avg_reward(variant):
-    clicks = int(r.get(f"clicks:{variant}") or 0)
-    views = int(r.get(f"views:{variant}") or 1)  # Avoid div by 0
-    return clicks / views
-
-def select_variant():
-    if random.random() < EPSILON:
-        return random.choice(VARIANTS)
-    rewards = {v: get_avg_reward(v) for v in VARIANTS}
-    return max(rewards, key=rewards.get)
+agentName = "eGreedy"
+agent = eGreedy(nArms = len(VARIANTS), redisClient = r)
 
 @app.get("/", response_class=HTMLResponse)
-async def show_page(request: Request):
-    variant = select_variant()
-    r.incr(f"views:{variant}")
+async def showPage(request: Request):
+    variant = VARIANTS[agent.selectArm()]
+    r.incr(f"{agentName}:arm:{variant}:views")
     return templates.TemplateResponse("index.html", {"request": request, "variant": variant})
 
 @app.get("/click/{variant}")
-async def track_click(variant: str):
+async def trackClick(variant: str):
     if variant in VARIANTS:
-        r.incr(f"clicks:{variant}")
+        reward = 1
+        agent.update(VARIANTS[variant], reward)
     return RedirectResponse("/")
 
 @app.get("/stats")
 def stats():
     return {
         v: {
-            "clicks": int(r.get(f"clicks:{v}") or 0),
-            "views": int(r.get(f"views:{v}") or 0),
-            "ctr": round(get_avg_reward(v), 4)
+            "clicks": int(r.get(f"{agentName}:arm:{v}:count") or 0),
+            "views": int(r.get(f"{agentName}:arm:{v}:views") or 0),
+            "ctr": round(r.get(f"{agentName}:arm:{v}:count") / int(r.get(f"{agentName}:arm:{v}:views") or 1), 2)
         }
-        for v in VARIANTS
+        for v in VARIANTS.values()
     }
