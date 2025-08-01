2a37e4687ecf4a30dae122f1db2dead0c740272f
vizz1234 <vishdd4@gmail.com>
2025-08-01 23:59:39 +0530

    completed epsilon greedy

--- eGreedy.py
+++ eGreedy.py
@@ -2,35 +2,39 @@ import random
 
 class eGreedy:
 
-    def __init__(self, nArms, epsilon = 0.1, redisClient = None):
+    def __init__(self, nArms, epsilon = 0.1, redisClient = None, initial = 5):
 
         self.nArms = nArms
         self.epsilon = epsilon
         self.redisClient = redisClient
+        self.initial = initial
 
-        for i in range(nArms):
+        for i in range(self.nArms):
             key = f"eGreedy:arm:{i}"
             if not self.redisClient.exists(key):
-                self.redisClient.hset(key, mapping = {"views": 0, "count": 0, "totalReward": 0})
+                self.redisClient.hset(key, mapping = {"views": 0, "count": 0, "avgReward": self.initial})
     
     def selectArm(self):
         if random.random() < self.epsilon:
-            return random.choice(self.nArms)
-
+            armExplored = random.choice(list(range(self.nArms)))
+            print(f"Exploring: {armExplored}")
+            return armExplored
+        print("Exploitation")
         # Exploitation
         bestArm = None
         bestValue = float('-inf')
         for arm in range(self.nArms):
-            stats = self.redis.hgetall(f"eGreedy:arm:{arm}")
-            count = int(stats.get(b'count', 0))
-            rewardSum = float(stats.get(b'totalReward', 0.0))
-            avgReward = rewardSum / count if count > 0 else 0.0
+            stats = self.redisClient.hgetall(f"eGreedy:arm:{arm}")
+            count = int(stats.get('count', 0))
+            views = int(stats.get('views', 0))
+            avgReward = (count + self.initial) / views if views > 0 else self.initial
             if avgReward > bestValue:
                 bestValue = avgReward
                 bestArm = arm
+        print(f'Exploiting: {bestArm}')
         return bestArm
 
     def update(self, arm, reward):
         key = f"eGreedy:arm:{arm}"
-        self.redis.hincrby(key, "count", 1)
-        self.redis.hincrbyfloat(key, "totalReward", reward)
+        self.redisClient.hincrby(key, "count", 1)
+        self.redisClient.hincrbyfloat(key, "totalReward", reward)
--- main.py
+++ main.py
@@ -9,34 +9,37 @@ r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 templates = Jinja2Templates(directory="templates")
 
 VARIANTS = {
-    "A": 0,
-    "B": 1,
-    "C": 2
+    0: "A",
+    1: "B",
+    2: "C"
 }
+reverseVARIANTS = {v: k for k, v in VARIANTS.items()}
 EPSILON = 0.1
 agentName = "eGreedy"
 agent = eGreedy(nArms = len(VARIANTS), redisClient = r)
 
 @app.get("/", response_class=HTMLResponse)
 async def showPage(request: Request):
-    variant = VARIANTS[agent.selectArm()]
-    r.incr(f"{agentName}:arm:{variant}:views")
+    action = agent.selectArm()
+    variant = VARIANTS[action]
+    r.hincrby(f"{agentName}:arm:{action}", "views", 1)
     return templates.TemplateResponse("index.html", {"request": request, "variant": variant})
 
 @app.get("/click/{variant}")
 async def trackClick(variant: str):
-    if variant in VARIANTS:
+    if variant in VARIANTS.values():
         reward = 1
-        agent.update(VARIANTS[variant], reward)
+
+        agent.update(reverseVARIANTS[variant], reward)
     return RedirectResponse("/")
 
 @app.get("/stats")
 def stats():
     return {
         v: {
-            "clicks": int(r.get(f"{agentName}:arm:{v}:count") or 0),
-            "views": int(r.get(f"{agentName}:arm:{v}:views") or 0),
-            "ctr": round(r.get(f"{agentName}:arm:{v}:count") / int(r.get(f"{agentName}:arm:{v}:views") or 1), 2)
+            "clicks": int(r.hget(f"{agentName}:arm:{v}", 'count') or 0),
+            "views": int(r.hget(f"{agentName}:arm:{v}", 'views') or 0),
+            "estimate": round((int(r.hget(f"{agentName}:arm:{v}", 'count') or 0) + agent.initial)/ int(r.hget(f"{agentName}:arm:{v}", 'views') or 1), 2)
         }
-        for v in VARIANTS.values()
+        for v in VARIANTS
     }
